58595d1453ecb380464ac2f56ba693e5f66c9c41
Bo-Yan Huang <r95222023@gmail.com>
2024-08-03 21:37:10 +0800

    added autogen and llamaindex examples

---
+++ Autogen/backend_dir/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import yfinance as yf
+
+app = FastAPI()
+
+@app.get("/cdr_daily_spread")
+async def calculate_daily_spread():
+    cdr = yf.Ticker("CDR.WA")
+    today_data = cdr.history(period="1d")
+    spread = ((today_data["High"] - today_data["Low"]) / today_data["Low"]) * 100
+    return spread
